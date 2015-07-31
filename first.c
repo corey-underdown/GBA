e280b32692518455872d23520f8973e8fcac6716
Silcoish <110938@student.sae.edu.au>
2015-07-31 21:27:33 +1000

    First changes to defines
    Changes to defines instead of raw mem locations

--- first.c
+++ first.c
@@ -4,12 +4,9 @@ int main()
 {
     *(unsigned int*)MEM_IO = 0x0403;
 
-    ((unsigned short*)0x06000000)[120+80*240] = 0x001F;
-    ((unsigned short*)0x06000000)[136+80*240] = 0x03E0;
-    ((unsigned short*)0x06000000)[120+96*240] = 0x7C00;
-
-    ((unsigned short*)0x06000000)[136+96*240] = 0x001F;
-    ((unsigned short*)0x06000000)[140+96*240] = 0x001F;
+    ((unsigned short*)MEM_VRAM)[120+80*240] = 0x001F;
+    ((unsigned short*)MEM_VRAM)[136+80*240] = 0x03E0;
+    ((unsigned short*)MEM_VRAM)[120+96*240] = 0x7C00;
 
     
 

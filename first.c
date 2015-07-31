df1a81ad0df5c95a846a364a06240b4cdb6eb355
Silcoish <110938@student.sae.edu.au>
2015-07-31 22:01:09 +1000

    Loops over the screen draws white pixels
    Draws white pixels on the screen. And not even for the whole screen!
    Truly a break through.

--- first.c
+++ first.c
@@ -1,20 +1,22 @@
+#include <stdio.h>
 #include "toolbox.h"
 
 int main()
 {
     *(unsigned int*)MEM_IO = 0x0403;
 
-    ((unsigned short*)MEM_VRAM)[120+80*240] = 0x001F;
-    ((unsigned short*)MEM_VRAM)[136+80*240] = 0x03E0;
-    ((unsigned short*)MEM_VRAM)[120+96*240] = 0x7C00;
-
-    
-
-    while(1);
-
-
-
-
+    while(1)
+    {
+		int i;
+	    int j;
+	    for(i = 0; i < SCREEN_WIDTH; i++)
+	    {
+	    	for(j = 0; j < SCREEN_HEIGHT; j++)
+	    	{
+	    		((unsigned short*)MEM_VRAM)[i+j*SCREEN_HEIGHT] = CLR_WHITE;
+	    	}
+	    }
+    }
 
     return 0;
 }

33a44f3687d73b726512911400319f013b4534f1
CSchnitzerling <110841@student.sae.edu.au>
2015-07-31 20:42:01 +1000

    Test Case Commit

---
+++ first.c
@@ -0,0 +1,15 @@
+int main()
+{
+    *(unsigned int*)0x04000000 = 0x0403;
+
+    ((unsigned short*)0x06000000)[120+80*240] = 0x001F;
+    ((unsigned short*)0x06000000)[136+80*240] = 0x03E0;
+    ((unsigned short*)0x06000000)[120+96*240] = 0x7C00;
+
+    ((unsigned short*)0x06000000)[136+96*240] = 0x001F;
+    ((unsigned short*)0x06000000)[140+96*240] = 0x001F;
+
+    while(1);
+
+    return 0;
+}

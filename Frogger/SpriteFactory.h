0a7f14cc8b7eb09ca5bb1d5a87485f5e38241a51
Silcoish <110938@student.sae.edu.au>
2015-08-06 16:49:12 +1000

    More testing. No avail.
    Tried rendering a single sprite, and couldn't without changing the
    render mode. Perhaps that is the problem?

--- Frogger/SpriteFactory.h
+++ Frogger/SpriteFactory.h
@@ -5,26 +5,6 @@
 #include "toolbox.h"
 
 
-typedef struct 
-{
-	unsigned char y;
-	unsigned char enableRotation:1;
-	unsigned char dubScale:1;
-	unsigned char alpha:2;
-	unsigned char mosaic:1;
-	unsigned char colourMode:1;//16 or 256 colour
-	unsigned char shape:2;//Square or Rect
-	unsigned short x:9;
-	unsigned char special:5;//DEpending on prvious values this will change.
-	unsigned char size:2;//0 8 16 32 64 depending on the sprite size
-	unsigned short tileIndex:10;//first tile in tile array.
-	unsigned char priority:2;
-	unsigned char pallet:4;
-	unsigned short rotation;//actual rotation
-}SpriteData;
-
-
-
 typedef struct
 {
 	SpriteData ghostOAM[128];
--- Frogger/main.c
+++ Frogger/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include "toolbox.h"
 #include "images/TestImage.h"
-#include "SpriteFactory.h"
+//#include "SpriteFactory.h"
+ 
+typedef struct 
+{
+	unsigned char y;
+	unsigned char enableRotation:1;
+	unsigned char dubScale:1;
+	unsigned char alpha:2;
+	unsigned char mosaic:1;
+	unsigned char colourMode:1;//16 or 256 colour
+	unsigned char shape:2;//Square or Rect
+	unsigned short x:9;
+	unsigned char special:5;//DEpending on prvious values this will change.
+	unsigned char size:2;//0 8 16 32 64 depending on the sprite size
+	unsigned short tileIndex:10;//first tile in tile array.
+	unsigned char priority:2;
+	unsigned char pallet:4;
+	unsigned short rotation;//actual rotation
+}SpriteData;
 
 typedef struct 
 {
@@ -16,25 +34,42 @@ int main()
 	
 	//Enable Sprites
     REG_DISPCNT = 0x1f00;
+	//REG_DISPCNT = DCNT_MODE3 | DCNT_BG2;
 
 	waitVBlank();
 
  	//HACK to create temporary tile
-	memcpy(((void*)TILE_LOCATION), (void*)BitmapA,32);  
+	memcpy(((void*)SPRITE_TILEDATA), (void*)Bitmap,32);  
+
+	SpriteData mysp;
+    mysp.x = 30;
+    mysp.y = 20;
+   	mysp.enableRotation = 0;
+	mysp.dubScale = 0;
+	mysp.alpha = 0;
+	mysp.mosaic = 0;//off
+	mysp.colourMode = 0;//16bit
+	mysp.shape = 0; //square
+	mysp.special = 0;
+	mysp.size = 0;// = 8 pixels wide;
+	mysp.tileIndex = 0;	
+	mysp.priority = 0;
+	mysp.pallet = 0;
+	mysp.rotation = 0;
 
 	//HACK create palette
 	shortCopy((u16*)SPRITE_PAL_DATA, (u16*)Palette, 256); 
 
 	//set background palette  
-	shortCopy((void*)TILE_PAL_DATA, (void*)Palette, 256);
+	//shortCopy((void*)TILE_PAL_DATA, (void*)Palette, 256);
 
-	TileData tData;
+	/*TileData tData;
 	tData.tile = 0;
 	tData.horizontalFlip = 0;
 	tData.verticalFlip = 0;
-	tData.palette = 1;
+	tData.palette = 0;*/
 
-	shortCopy((u16*)SPRITE_TILEDATA, (u16*)&tData, 32);
+	//shortCopy((u16*)MEM_VRAM, (u16*)&tData, 32);
 
 	//Sprite Factory Using==================================================
 	//SpriteFactory* spf = Ghost_OAM_Create();
@@ -82,6 +117,8 @@ int main()
 
 	*/
 
+	((unsigned short*)TILE_LOCATION)[120+80*240] = 0x001F;
+
 	int x=0;    
 	
 	while(1)
@@ -96,6 +133,8 @@ int main()
 		
 		waitVBlank();
 		//Ghost_OAM_CopytoOAM(spf);
+		//shortCopy((u16*)SPRITE_TILEDATA, (u16*)&tData, 32);
+		
 		waitVDraw();
 
 		//Gregs Code==========================================

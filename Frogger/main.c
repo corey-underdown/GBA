1b3b9b9387b85ecafd37b10f433705a8227fe53d
Silcoish <110938@student.sae.edu.au>
2015-08-06 12:04:32 +1000

    Input manager
    Just ise the isKeyDown function and pass the key you want. Bools are not
    a thing in c without a std library, so I return an int instead.

--- Frogger/main.c
+++ Frogger/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include "toolbox.h"
+#include "input.h"
 #include "images/TestImage.h"
-#include "SpriteFactory.h"
 
+typedef struct 
+{
+	unsigned char y;
+	unsigned char enableRotation:1;
+	unsigned char dubScale:1;
+	unsigned char alpha:2;
+	unsigned char mosaic:1;
+	unsigned char colourMode:1;//16 or 256 colour
+	unsigned char shape:2;//Square or Rect
+	unsigned short x:9;
+	unsigned char special:5;//DEpending on prvious values this will change.
+	unsigned char size:2;//8 16 32 64 depending on the sprite size
+	unsigned short tileIndex:10;//first tile in tile array.
+	unsigned char priority:2;
+	unsigned char pallet:4;
+	unsigned short rotation;//acutal rotation
+}SpriteData;
 
 void waitVBlank()
 {
@@ -27,8 +44,6 @@ void shortCopy(u16 *dest, u16 *src, int shortCount)
 
 int main()
 {
-
-	int i;
 	
 	//Enable SPtrites
     REG_DISPCNT = 0x1f00;
@@ -62,17 +77,40 @@ int main()
 	//set background palette
 	((volatile unsigned short*)(0x05000000))[0] = CLR_RED;
 
-	int x=0;    
+	int x = mysp.x;  
+	int y = mysp.y;  
 	
 	while(1)
     {
 
-
-		mysp.x = x;
-		x++;
-		if(x>240)
+    	//Logic
+    	
+    	if(isKeyDown(KEY_UP) == 1)
+    	{
+    		y--;
+    	}else if(isKeyDown(KEY_DOWN) == 1)
+    	{
+    		y++;
+    	}else if(isKeyDown(KEY_RIGHT) == 1)
+    	{
+    		x++;
+    	}else if(isKeyDown(KEY_LEFT) == 1)
+    	{
+    		x--;
+    	}
+    	mysp.x = x;
+    	mysp.y = y;
+		if(x > SCREEN_WIDTH)
 			x=0;
-		
+		else if(x < 0)
+			x = SCREEN_WIDTH - 1;
+
+		if(y < 0)
+			y = SCREEN_HEIGHT - 1;
+		else if(y > SCREEN_HEIGHT)
+			y = 0;
+    	
+
 		waitVBlank();
 		shortCopy((u16*)SPRITE_OAM, (u16*)&mysp,4);
 		waitVDraw();

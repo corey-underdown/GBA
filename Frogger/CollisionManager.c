c1f3487df717d5e0da23cf8f7ee07d8bc70dfc84
Silcoish <110938@student.sae.edu.au>
2015-08-18 15:04:09 +1000

    Collision manager appears to detect obj collision
    It spits out an index of the sprite that it hit into the debug log if it
    detects a collision.

--- Frogger/CollisionManager.c
+++ Frogger/CollisionManager.c
@@ -3,14 +3,14 @@
 void DetectCollision(GameObject* frogger){
 	for(int i = 0; i < 127; i++)
 	{
-		if(g_GOFactory.GOList[i].alive == TRUE)
+		if(g_GOFactory.GOList[i].alive == TRUE && i != 0)
 		{
-			if(g_GOFactory.GOList[i].sprite->x < frogger->sprite->x + 16 &&
-			g_GOFactory.GOList[i].sprite->x + 16 > frogger->sprite->x &&
-			g_GOFactory.GOList[i].sprite->y < frogger->sprite->y + 16 &&
-			g_GOFactory.GOList[i].sprite->y + 16 > frogger->sprite->y)
+			if(g_GOFactory.GOList[i].sprite->x < frogger->sprite->x + 8 &&
+			g_GOFactory.GOList[i].sprite->x + 8 > frogger->sprite->x &&
+			g_GOFactory.GOList[i].sprite->y < frogger->sprite->y + 8 &&
+			g_GOFactory.GOList[i].sprite->y + 8 > frogger->sprite->y)
 			{
-				//PrintText("Collision");	
+				PrintTextInt(i);	
 			}
 		}
 	}
